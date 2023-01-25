1ca6d288fb62369af222554564803d74d7525472
Wendel Paes <107985828+WendelPaesx@users.noreply.github.com>
2023-01-25 03:38:25 -0300

    .

---
+++ app/cep.js
@@ -0,0 +1,10 @@
+console.log("mandando oi pro amigo");
+    function mandaMensagem(){
+        console.log("tudo bem ?");
+        console.log("vou te mandar uma solicitacao");
+        console.log("solicitacao recebida");
+
+    }
+
+    mandaMensagem()
+    console.log("tchayu!");
